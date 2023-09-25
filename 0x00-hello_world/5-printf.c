cc9c40c4127f5a6320e320e746c527e7d6ea0fb9
Tamactii <appiahemerald6@gmail.com>
2023-09-25 00:54:43 -0700

    added task 5 answer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * prints with proper grammar, but the outcome is a piece of art
+ * , followed by a new line
+ *
+ * Return: 0 always sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
