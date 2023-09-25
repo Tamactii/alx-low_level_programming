98ea427b6e53c3039d2df430af8909ba318836e9
Tamactii <appiahemerald6@gmail.com>
2023-09-25 00:32:01 -0700

    made changes to conform with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Prints ""Programming is like building a multingual puzzle"", followed by a new line
- * return always 0
+ * prints programming is like building
+ * multilingual puzzle, followed
+ * by a new line
+ *
+ * Return: 0 always sucess
  */
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
