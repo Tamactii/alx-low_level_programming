01dfd2a7d04795bab40d32b08796adc8e47ce3af
Tamactii <appiahemerald6@gmail.com>
2023-09-25 00:24:48 -0700

    added \" to the text line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * Prints "Programming is like building a multingual puzzle"
+ * main - entry point
+ * Prints ""Programming is like building a multingual puzzle"", followed by a new line
  * return always 0
  */
 int main(void)
 {
 	
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
